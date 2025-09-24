886cad7ab0df7f14932a23f772215c9e5b13fa65
kuroyuki.dev <kuroyuki.dev@gmail.com>
2025-09-24 19:21:38 +0000

    fix missing Copyright Year injection code

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function getCurrentYear() {
     if (yearSpan) {
         yearSpan.textContent = new Date().getFullYear();
     }
-    console.log('DOM fully loaded and parsed.');
+    //console.log('DOM fully loaded and parsed.');
 }
 
 (function () {
-    console.log('main.js module loaded and executed.');
+    //console.log('main.js module loaded and executed.');
     getCurrentYear();
 })()
